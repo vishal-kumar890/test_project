1213cfeeb08d4c3ff78d75f718f4f6a654c439d4
vishal kumar <vishal.k@summence.com>
2022-05-18 13:18:29 +0530

    Test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os, sqlite3
 
 app = FastAPI()
 # app.scheduler = sch
-app.add_middleware(ApilyticsMiddleware, api_key=os.getenv("792fcbc9-89e6-448b-acbb-440721f66816"))
+app.add_middleware(ApilyticsMiddleware, api_key="792fcbc9-89e6-448b-acbb-440721f66816")
 
 app.mount("/page_assets", page_assets, name="page_assets")
 
